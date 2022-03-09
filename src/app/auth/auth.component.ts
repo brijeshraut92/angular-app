d4e7c5e5c3ba2415461df2e1e31d618487a380d1
Brijesh <brijeshraut92@gmail.com>
2022-03-09 13:28:37 +0530

    initial_changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-// import { AuthService } from "./auth.service";
+import { AuthService } from "./auth.service";
 
 
 @Component({
@@ -9,9 +9,10 @@ import { Router } from "@angular/router";
     templateUrl: './auth.component.html'
 })
 export class AuthComponent {
-    isLoginMode = true; 
-    constructor(private router: Router) { }
-    // isLoginMode = true;
+    // isLoginMode = true; 
+    constructor(private router: Router,
+        private authService: AuthService) { }
+    isLoginMode = true;
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -19,23 +20,23 @@ export class AuthComponent {
     }
     onSubmit(form: NgForm) {
         console.log(form.value)
-        // if (!form.valid) {
-        //     return;
-        // }
-        // const email = form.value.email;
-        // const password = form.value.password;
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
 
-        // if (this.isLoginMode) {
-            //..//  
-        // } else {
-            // this.authService.signup(email, password).subscribe(
-            //     resData => {
-            //         console.log(resData);
-            //     }, 
+        if (this.isLoginMode) {
+            
+        } else {
+            this.authService.signup(email, password).subscribe(
+                resData => {
+                    console.log(resData);
+                }, 
                 // error => {
                 //     console.log(error);
                 // }
-                // );
+                );
 
             form.reset();
             this.router.navigate(['home']);
@@ -43,4 +44,4 @@ export class AuthComponent {
         }
     }
 
-// }
+}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http'
 
 //optional//
-interface AuthResponseData {
-    idToken: string 
-    IDToken: string
-    email: string
-    refreshToken: string
-    expiresIn: string
-    localId: string
-}
+// interface AuthResponseData {
+//     idToken: string 
+//     IDToken: string
+//     email: string
+//     refreshToken: string
+//     expiresIn: string
+//     localId: string
+// }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
     constructor(private http: HttpClient) { }
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>(
+        return this.http.post(
             'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDDKPXJp848uvo9LXgtZXMAYec_5NHDLW8',
             {
                 email: email,
